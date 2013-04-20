bd62ef736bce87db9c8193017b1e9713635dedc2
DavidGarciaBravo <soufianiku@gmail.com>
2013-04-21 01:26:40 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 	define('TITLE_PAGE' , 'My application');
 	define('URL','http://tutorialesdm.com/');
 	define('favIco','null');
-	define('descripcion','Este MVC esta basado en un framework basico ya que solo lo usare yo mismo');
+	define('descripcion','TutorialesDM :D');
 
 
